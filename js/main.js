ac6c70b0775388406199b9594d215458d6d9d09a
Ryan Booth <ryanboothforever@gmail.com>
2021-08-19 22:20:02 -0500

    Snippet added to head

--- js/main.js
+++ js/main.js
@@ -1,3 +1,1 @@
 //To incorporate: When you click "add to cart" an icon appears with a checkout/items in basket (counter)
-
-UA.then((sdk) => console.log(sdk.getChannel().then((channel) => channel.id)));
